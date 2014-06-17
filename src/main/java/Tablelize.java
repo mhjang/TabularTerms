a7fac4f494e438d469f70fb546466ef5e92980d6
mhjang <mhjang@cs.umass.edu>
2014-06-16 23:45:23 -0400

    applied bootstrap css

--- src/main/java/Tablelize.java
+++ src/main/java/Tablelize.java
@@ -19,7 +19,7 @@ import java.util.Map;
 public class Tablelize {
      static int lineIndex = 0;
      static DecimalFormat numberFormat = new DecimalFormat(("#.000"));
-     static String[] colors = {"yellow", "Coral", "orange", "DarkSalmon", "DarkTurquoise", "GreenYellow", "lime", "teal", "Pink", "Salmon", "SlateBlue", "Skyblue", "RoyalBlue", "Violet", "Tomato"};
+     static String[] colors = {"#ffeda3", "#428bca", "#5cb85c", "#5bc0de", "#f0ad4e", "#d9534f"};
 
      /**
        *
@@ -29,7 +29,7 @@ public class Tablelize {
      public static void main(String[] args) throws IOException {
          String dir = "/Users/mhjang/Documents/workspace/TeachingTest/testdata";
          int columnNum = 5, k = 10;
-         String outFileName = "output.html";
+         String outFileName = "./output/output.html";
 
          /**
           *  process the arguments
@@ -54,8 +54,26 @@ public class Tablelize {
              tfidf.calulateTFIDF(TFIDFCalculator.LOGTFIDF, dir, Tokenizer.UNIGRAM, false);
              DocumentCollection dc = tfidf.getDocumentCollection();
 
+         /***
+          * add heading
+          */
+            bw.write("<!DOCTYPE html>\n" +
+                    "<html lang=\"en\">\n" +
+                    "  <head>\n" +
+                    "    <meta charset=\"utf-8\">\n" +
+                    "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+                    "    <title> Term Tablize Ver 1.0 </title>\n" +
+                    "\n" +
+                    "    <!-- Bootstrap -->\n" +
+                    "    <link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">\n" +
+                    "  </head>\n");
+
              bw.write("<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script> \n");
-             bw.write("<table border=\"0\" width=\"1051\"> \n");
+         bw.write("<center><H2> High TF/IDF Term Tablization </H2> <hr> <br><br>");
+        // bw.write("<span class=\"label label-default\">View All</span>");
+
+         bw.write("<table border=\"0\" width=\"1051\"> \n");
 
 
              HashMap<String, Document> documentSet = dc.getDocumentSet();
@@ -65,20 +83,19 @@ public class Tablelize {
                  generateHTMLTable(doc.getName(), topRankedTerms, colors[0], bw);
              }
 
-             bw.write("<H2> High TF/IDF Term Tablization </H2>");
              bw.write("</table> \n");
              bw.write("<script>\n" +
                      "$(document).ready(function(){\n" +
                      "var idx = 0;" +
-                     "var colour = ['yellow', 'Coral', 'orange', 'DarkSalmon', 'DarkTurquoise', 'GreenYellow', 'lime', 'teal', 'Pink', 'Salmon', 'SlateBlue', 'Skyblue', 'RoyalBlue', 'Violet', 'Tomato'];\n" +
+                     "var colour = [\"#ffeda3\", \"#428bca\", \"#5cb85c\", \"#5bc0de\", \"#f0ad4e\", \"#d9534f\"];\n" +
                      "   $(\"td\").click(function() {\n" +
                      "        var rand = Math.floor(Math.random() * colour.length);\n" +
                      "        var classId = $(this).attr(\"class\");" +
-                     "        $(\".\"+classId).css(\"background-color\", colour[rand]);\n" +
+                     "        if ($(\".\"+classId).css(\"background-color\") == 'rgba(0, 0, 0, 0)') \n" +
+                     "        \t$(\".\"+classId).css(\"background-color\", colour[rand]);\n" +
+                     "        else $(\".\"+classId).css(\"background-color\", 'rgba(0, 0, 0, 0)');\n" +
                      "        });\n" +
-                     "});\n" +
-                     "</script>");
-
+                     "}); \n </script>");
              bw.close();
 
 
@@ -90,19 +107,19 @@ public class Tablelize {
 
         public static void generateHTMLTable(String tableName, LinkedList<Map.Entry<String, Double>> elements, String tableColor, BufferedWriter bw) throws IOException {
             if(lineIndex % 5 == 0) {
-                bw.write("</tr>");
-                bw.write("<tr>");
+                if(lineIndex != 0) bw.write("</tr> \n");
+                bw.write("<tr> \n");
             }
-            bw.write("<td>");
-            bw.write("<table border=\"0\" cellpadding=\"2\" cellspacing=\"0\" style=\"width:250px\" text-align: center;\">");
-            bw.write("<tr> <td style=\"background-color: "+tableColor +" id=\""+ tableName + "\"><b>" + tableName + "</b></td></tr>");
+            bw.write("<td>\n");
+            bw.write("<table class = \"table table-condensed\" border=\"0\" cellpadding=\"2\" cellspacing=\"0\" style=\"width:250px\" text-align: center;\"> \n");
+            bw.write("<tr> <td style=\"background-color: "+tableColor +"; id=\""+ tableName + "\"><b>" + tableName + "</b></td></tr> \n");
             for(Map.Entry<String, Double> ele : elements) {
                 String[] tokens = ele.getKey().split(" ");
 
-                bw.write("<tr> <td class=\""+tokens[0]+"\">" + ele.getKey() + "("+ numberFormat.format(ele.getValue()) +") </td></tr>");
+                bw.write("<tr> <td class=\""+tokens[0]+"\">" + ele.getKey() + "("+ numberFormat.format(ele.getValue()) +") </td></tr> \n");
             }
-            bw.write("</table>");
-            bw.write("</td>");
+            bw.write("</table> \n");
+            bw.write("</td> \n");
             lineIndex++;
         }
 
